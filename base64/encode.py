d4f1dec078abb78e22bd1a3e4a157358c558b9be
guangqianzhang <gqzhang77@163.com>
2023-04-27 19:58:26 +0800

    for tsv file

--- base64/encode.py
+++ base64/encode.py
@@ -8,6 +8,7 @@ import json
 from tsv import TSVFile
 import cv2
 tsv_file_path = '/home/cqjtu/Documents/cc3m/train-0000.tsv'
+tsv_depth_file_path = '/home/cqjtu/Documents/cc3m_depth/train-0000.tsv'
 
 
 def decode_base64_to_pillow(image_b64):
@@ -41,21 +42,29 @@ def decode_item(item):
     return item
 
 
-index = 0
+
 tsvfile = TSVFile(tsv_file_path)
-for idx in tsvfile:
-    _, item=idx
+tsvdepthfile = TSVFile(tsv_depth_file_path)
+for idx in zip(tsvfile,tsvdepthfile):
+    _, item=idx[0]
+    _,itemdepth=idx[1]
+
+    itemdepth= decode_item_depth(itemdepth)
+    depth_data_id,  depth_img = itemdepth.values()
+    np_Dimage = np.array(depth_img)
+    np_Dimage = cv2.cvtColor(np_Dimage, cv2.COLOR_RGB2BGR)
+
     item= decode_item(item)
     data_id,img,filename,caption,ann=item.values()
     bbox=ann[0]['bbox']
-    pil_image=item['image']
-    caption=item['caption']
+    assert (len(bbox)==4)
+    label=str(data_id)+':'+caption
     np_image = np.array(img)
-
-    # Convert the Numpy array to an OpenCV image
     opencv_image = cv2.cvtColor(np_image, cv2.COLOR_RGB2BGR)
-    cv2.putText(opencv_image, str(data_id)+':'+caption, (15, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (100,0,255), 1)
-    cv2.rectangle(opencv_image, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0,0,255), 2)
-    cv2.imshow('outImage',opencv_image)
+    if label is not None: cv2.putText(opencv_image, label, (15, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (100,0,255), 1)
+    if bbox is not None: cv2.rectangle(opencv_image, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0,0,255), 2)
+
+    cv2.imshow('original Image',opencv_image)
+    cv2.imshow('depth Image',np_Dimage)
     cv2.waitKey(0)
 cv2.destroyAllWindow()
